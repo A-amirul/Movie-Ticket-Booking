6b5f96f9a53db68e80ece07cc316dbc410740ca2
A-amirul <amirulislamamir412@gmail.com>
2023-03-29 15:38:10 +0600

    watch time added to the side card

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,20 @@ import Home from './components/Home/Home'
 import SideCart from './components/SideCart/SideCart'
 
 function App() {
+  const [watchTime, setWatchTime] = useState(0);
   const handleWatchTime = (time) => {
     // age theke watchTime set kora ache naki seta dekhar jonno
     const previousWatchTime = JSON.parse(localStorage.getItem('watchTime'));
     if (previousWatchTime) {
       // jodi thake tahole ager time er sathe bortoman time add korbo
       const updateTime = previousWatchTime + time;
-      localStorage.setItem('watchTime',updateTime);
+      localStorage.setItem('watchTime', updateTime);
+      setWatchTime(updateTime);
     }
     else {
       // jodi na thake time take set korbo
       localStorage.setItem('watchTime', time);
+      setWatchTime(time);
     }
   }
   return (
@@ -26,8 +29,10 @@ function App() {
         <div className="home-container col-md-8">
           <Home handleWatchTime={handleWatchTime}></Home>
         </div>
-        <div className="sideCart-container card col-md-3 bg-secondary">
-          <SideCart></SideCart>
+        <div className="sideCart-container card col-md-3">
+          <SideCart
+            watchTime={watchTime}
+          ></SideCart>
 
         </div>
       </div>
--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const SideCart = () => {
+const SideCart = ({ watchTime }) => {
+	const [time,setTime] = useState(watchTime);
+	useEffect(() => {
+		const GetWatchTimeLocalStorage = localStorage.getItem('watchTime');
+		setTime(GetWatchTimeLocalStorage);
+	}, [watchTime]);
 	return (
-		<div>
-			side card here
+		<div className=''>
+			<h1 className='text-center'>My Cart</h1>
+			<div className='text-center mt-5'>
+				<p>Total watch Time</p>
+				<input type="text" value={time} name="" id="" />
+			</div>
+
 		</div>
 	);
 };
